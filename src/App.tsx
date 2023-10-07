34c0f2c877cd546177a6a3f2f7f9919dd6baa746
Jungwoo <evencoding@gmail.com>
2023-10-08 03:52:33 +0900

    refactor: 초기값을 values에 할당, TODO 주석 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-// import { useForm } from './libs/useForm';
-import { useForm } from 'react-hook-form';
+import { useForm } from './libs/useForm';
+// import { useForm } from 'react-hook-form';
 import './App.css';
 
 interface Values {
@@ -12,7 +12,7 @@ function App() {
 
   // 어떻게 타입 추론이 가능하게 할 수 있을까?
   const onSubmit = (values: Values) => {
-    console.log(values);
+    console.log(typeof values.age);
   };
 
   return (
@@ -20,11 +20,9 @@ function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           {...register('age', {
-            // onMountFocus: true,
-            // value: true,
-            // string이 아닌 타입이라면 setValueAs를 반드시 넣어줘야해
-            // 그렇지 않으면 submit 시, Values 타입과 일치하지 않는 문제가 발생해
-            // setValueAs: (value) => Number(value),
+            onMountFocus: true,
+            value: 3,
+            setValueAs: (value) => Number(value),
           })}
         />
       </form>
--- src/libs/useForm/useForm.ts
+++ src/libs/useForm/useForm.ts
@@ -22,16 +22,28 @@ const useForm = <T = FieldValues>(): UseFormReturnType<T> => {
       ref: (element: FieldElementType) => {
         if (!element) return;
 
-        if (options?.value) element.value = String(options?.value);
+        // TODO nested 처리
+        // set(inputRefs.current, nested 객체, element);
+
         inputRefs.current[name] = element;
+
+        if (options?.value) {
+          element.value = String(options?.value);
+
+          // TODO nested 처리
+          // set(valuesRef.current, nested 객체 , options.value);
+
+          // Set Value
+          valuesRef.current[name as keyof T] = options.value as T[keyof T];
+        }
 
         if (options?.onMountFocus) inputRefs.current[name]?.focus();
       },
 
       onChange: ({ target: { name, value } }: React.ChangeEvent<FieldElementType>) => {
         // TODO 요효성 검사
-        // TODO values 업데이트 (어떻게 받은 타입에 맞게 업데이트 할 수 있을까?)
 
+        // Set Value
         const transformedValue = options?.setValueAs ? options.setValueAs(value) : value;
         valuesRef.current[name as keyof T] = transformedValue as T[keyof T];
       },
@@ -56,15 +68,3 @@ const useForm = <T = FieldValues>(): UseFormReturnType<T> => {
 };
 
 export default useForm;
-
-// 성공한 형태. 즉, values가 T가 되는 형태
-// const values: T = Object.entries(inputRefs.current).reduce((acc, [name, element]) => {
-//   acc[name as keyof T] = (element as FieldElementType)!.value as T[keyof T];
-//   return acc;
-// }, {} as T);
-
-// HTML 입력 필드가 항상 문자열 값을 반환한다는 것이 너무 타입 지정을 어렵게하네
-// 현재 Values의 타입의 age 프로퍼티는 number 타입이지만, ref의 element의 value에는 String 형식만이 들어갈 수 있기에 실제 Values의 타입과 일치하지 않아.
-// 이는 추후 submit 이벤트가 발생했을 때, 타입 불일치라는 결과를 가져올 수 있어.
-// 어떻게 해결할 수 있을까?
-// setValueAs로 해결하는 거 좋다 이거야. 하지만 사용자에게 알려야할거아니야 어떻게 알릴거지?
