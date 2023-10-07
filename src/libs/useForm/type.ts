3247399d4ccfea340ade8350f4bd6eef403b4ba4
Jungwoo <evencoding@gmail.com>
2023-10-08 03:29:14 +0900

    feat: onChange 이벤트 구현

--- src/libs/useForm/type.ts
+++ src/libs/useForm/type.ts
@@ -1,11 +1,47 @@
+import React from 'react';
+
 export type Noop = () => void;
 
 export type FieldElementType = HTMLInputElement | HTMLTextAreaElement | null;
 
+export type ValueTypes = string | number | boolean;
+
 export interface FieldValues {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [x: string]: any;
 }
 
 export type InputRefsType<T> = {
   [K in keyof T]?: FieldElementType;
+};
+
+export type Options = Partial<{
+  onErrorFocus: boolean;
+  onMountFocus: boolean;
+
+  required: boolean;
+  maxLength: number;
+  minLength: number;
+  max: number;
+  min: number;
+  pattern: RegExp;
+  validate: Noop;
+
+  value: ValueTypes;
+  setValueAs: (value: string) => ValueTypes;
+}>;
+
+export type HandleSubmitType<T> = (callback: (values: T) => void) => (e: React.FormEvent) => void;
+
+export type UseFormReturnType<T> = {
+  register: (
+    name: keyof T,
+    options?: Options
+  ) => {
+    name: string;
+    ref: (element: FieldElementType) => void;
+    onChange: (e: React.ChangeEvent<FieldElementType>) => void;
+  } | void;
+  handleSubmit: HandleSubmitType<T>;
+  errors: Partial<T>;
 };
--- src/libs/useForm/useForm.ts
+++ src/libs/useForm/useForm.ts
@@ -1,37 +1,70 @@
 import React, { useRef, useState } from 'react';
-import { FieldElementType, FieldValues, InputRefsType, Noop } from './type';
+import {
+  FieldElementType,
+  FieldValues,
+  HandleSubmitType,
+  InputRefsType,
+  Options,
+  UseFormReturnType,
+} from './type';
 
-const useForm = <T = FieldValues>(initValues?: Partial<T>) => {
+const useForm = <T = FieldValues>(): UseFormReturnType<T> => {
   const [errors, setErrors] = useState<Partial<T>>({});
   const inputRefs = useRef<InputRefsType<T>>({});
+  const valuesRef = useRef<Partial<T>>({});
 
-  const register = (name: keyof T) => {
+  const register = (name: keyof T, options?: Options) => {
     if (inputRefs.current?.[name]) return;
 
     return {
+      name: String(name),
+
       ref: (element: FieldElementType) => {
         if (!element) return;
 
-        if (initValues?.[name]) element.value = String(initValues?.[name]);
+        if (options?.value) element.value = String(options?.value);
         inputRefs.current[name] = element;
 
-        return inputRefs.current[name];
+        if (options?.onMountFocus) inputRefs.current[name]?.focus();
+      },
+
+      onChange: ({ target: { name, value } }: React.ChangeEvent<FieldElementType>) => {
+        // TODO 요효성 검사
+        // TODO values 업데이트 (어떻게 받은 타입에 맞게 업데이트 할 수 있을까?)
+
+        const transformedValue = options?.setValueAs ? options.setValueAs(value) : value;
+        valuesRef.current[name as keyof T] = transformedValue as T[keyof T];
       },
     };
   };
 
-  const handleSubmit = (callback: Noop) => (e: React.FormEvent) => {
+  const handleSubmit: HandleSubmitType<T> = (callback) => (e) => {
     e.preventDefault();
 
-    callback();
+    // TODO 유효성 검사
+
+    const values = valuesRef.current as T;
+
+    callback(values);
   };
 
   return {
     register,
     handleSubmit,
     errors,
-    values: inputRefs.current,
   };
 };
 
 export default useForm;
+
+// 성공한 형태. 즉, values가 T가 되는 형태
+// const values: T = Object.entries(inputRefs.current).reduce((acc, [name, element]) => {
+//   acc[name as keyof T] = (element as FieldElementType)!.value as T[keyof T];
+//   return acc;
+// }, {} as T);
+
+// HTML 입력 필드가 항상 문자열 값을 반환한다는 것이 너무 타입 지정을 어렵게하네
+// 현재 Values의 타입의 age 프로퍼티는 number 타입이지만, ref의 element의 value에는 String 형식만이 들어갈 수 있기에 실제 Values의 타입과 일치하지 않아.
+// 이는 추후 submit 이벤트가 발생했을 때, 타입 불일치라는 결과를 가져올 수 있어.
+// 어떻게 해결할 수 있을까?
+// setValueAs로 해결하는 거 좋다 이거야. 하지만 사용자에게 알려야할거아니야 어떻게 알릴거지?
