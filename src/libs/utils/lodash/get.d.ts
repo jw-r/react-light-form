b08bff4e31d2110b408f60bbbf70b7ac79d635cb
Jungwoo <evencoding@gmail.com>
2023-10-24 03:10:17 +0900

    feat: lodash의 get함수 copy 및 set과 get 유틸에 대한 타입 지정

---
+++ src/libs/utils/lodash/get.d.ts
@@ -0,0 +1,3 @@
+// get.d.ts
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function get(obj: object, path: string | any[], defaultValue?: any): object;
---
+++ src/libs/utils/lodash/set.d.ts
@@ -0,0 +1,3 @@
+// set.d.ts
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function set(obj: object, path: string | any[], value: unknown): object;
